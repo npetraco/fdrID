95026dce8ffe72e3290c418922b7f49bb88e49d6
npetraco <npetraco@gmail.com>
2015-06-17 21:56:51 -0400

    Added gaussian smearing for problem (-Infinity-ish) z-values corresponding to problem (0-ish) p-values

--- R/smear.pz.R
+++ R/smear.pz.R
@@ -2,7 +2,7 @@
 #' @title smear.extreme.nonnull.pvalues
 #' @description XXXX
 #' 
-#' infinite z-values
+#' @details deal with infinite z-values
 #'
 #' @param XXXX
 #' 
@@ -82,3 +82,85 @@ smear.extreme.nonnull.pvalues <- function(non.null.pvalues, upper.set.pvalue=1e-
   return(non.null.pvalue.replacement.info)
   
 }
+
+
+#--------------------------------------------
+#' @title smear.extreme.nonnull.zvalues
+#' 
+#' @description XXXX
+#' 
+#' @details deal with infinite z-values
+#'
+#' @param XXXX
+#' 
+#' @return XXXX
+#'
+#' @examples
+#' XXXX
+#--------------------------------------------
+smear.extreme.nonnull.zvalues <- function(non.null.pvalues, upper.set.zvalue=(-12), mu.factor=(-0.5), p.factor=0.95, plotQ=FALSE) {
+    
+  non.null.zvalues <- qnorm(non.null.pvalues)
+  
+  #indices of non-null p-values less than upper.set.pvalue. The p-values at these indices should be smeared out over a narrow negative z-interval:
+  pv.not.acceptable.idxs <- which(non.null.zvalues < upper.set.zvalue)
+  
+  smear.info.mat<-cbind(pv.not.acceptable.idxs, non.null.pvalues[pv.not.acceptable.idxs], non.null.zvalues[pv.not.acceptable.idxs])
+  colnames(smear.info.mat)<-c("pval idx", "pval to be replaced", "corresp zval")
+  print(smear.info.mat)
+  
+  #Look at the p and z values that are "ACCEPTABLE"
+  pv.accepable.idxs <- 1:length(non.null.pvalues)
+  pv.accepable.idxs <- pv.accepable.idxs[-pv.not.acceptable.idxs]
+  no.smear.info.mat <- cbind(pv.accepable.idxs, non.null.pvalues[-pv.not.acceptable.idxs], non.null.zvalues[-pv.not.acceptable.idxs])
+  colnames(no.smear.info.mat)<-c("pval idx", "pval to be kept", "corresp zval")
+  print(no.smear.info.mat)
+  
+  #sig <- abs(min(no.smear.info.mat[,3])/sigma.factor)
+  #mmu <- 6*min(no.smear.info.mat[,3])/sigma.factor
+  minz <- min(no.smear.info.mat[,3])
+  mmu <- minz + mu.factor
+  sig <- (-1*minz+mmu)/(sqrt(2) * erf.inv(1-2*p.factor))
+  
+  #Smear out -Inf z values (0 p-values) with a gaussian random sample centerd mu.factor to the left of the smallest 
+  #actual non -Inf z-value (minz, i.e. most negative, but > upper.set.zvalue) and with an sd that corresponds to a p-value for
+  #minz of p.factor.
+  z.zeros <- rnorm(length(pv.not.acceptable.idxs), mean = mmu, sd= sig)
+    
+  #Do the replacement:
+  non.null.zvalues.mod <- non.null.zvalues
+  non.null.zvalues.mod[pv.not.acceptable.idxs] <- z.zeros
+  
+  non.null.zvalue.replacement.info <- list(smear.info.mat, no.smear.info.mat, non.null.zvalues.mod)
+  
+  names(non.null.zvalue.replacement.info) <- c("Replacement z-value info", "Non-replacement z-value info", "Modified z-values")
+  
+  if(plotQ==TRUE) {
+    split.screen( figs = c( 1, 2 ) )
+    
+    screen(1)
+    hist(z.zeros, xlab="z", main="Replacement non-null z-values")
+    
+    screen(2)
+    hist(non.null.zvalues.mod, xlab="z", main="All non-null z-values")
+    
+    close.screen( all = TRUE )
+    
+    print(paste("Most neg. actual z-value computed:", min(no.smear.info.mat[,3])))
+    print(paste("mu:                               ", mmu))
+    print(paste("Sample mean:                      ", mean(z.zeros)))
+    print("================================================")
+    print(paste("sigma:                            ", sig))
+    print(paste("Sample sd:                        ", sd(z.zeros)))
+    
+  }
+  
+  return(non.null.zvalue.replacement.info)
+  
+  
+}
+
+#----------------------------------
+#Internal. Inverse error function
+#----------------------------------
+erf.inv <- function(x) qnorm((x + 1)/2)/sqrt(2)
