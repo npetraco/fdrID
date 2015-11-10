1369c86eefc2ea883471aa8616ffc546be293db9
npetraco <npetraco@gmail.com>
2015-11-10 07:04:34 -0500

    Added zooming function for fdr/tdr plots. Finished make.fdr.functions. It need speeding up.

--- R/make.fdr.functions.R
+++ R/make.fdr.functions.R
@@ -43,19 +43,46 @@ make.fdr.functions <- function(z.values, bin.midpoints, pct0=0.25, posterior.f.v
     stop("Interval Type Not Implemented!")
   }
   
+  #Local false discovery rate interpolation functions:
   fdr.means.func <- splinefun(bin.midpoints, posterior.fdr.means)
   fdr.medians.func <- splinefun(bin.midpoints, posterior.fdr.medians)
   
   upper.fdr.ci.func <- splinefun(bin.midpoints, cred.ints.fdrs[,2])
   lower.fdr.ci.func <- splinefun(bin.midpoints, cred.ints.fdrs[,1])
   
+  #Local true discovery rate interpolation functions:
+  tdr.means.func <- splinefun(bin.midpoints, 1 - posterior.fdr.means)
+  tdr.medians.func <- splinefun(bin.midpoints, 1 - posterior.fdr.medians)
+  
+  upper.tdr.ci.func <- splinefun(bin.midpoints, 1 - cred.ints.fdrs[,2])
+  lower.tdr.ci.func <- splinefun(bin.midpoints, 1 - cred.ints.fdrs[,1])
+  
   if(plotQ==TRUE) {
-    plot(bin.midpoints, cred.ints.fdrs[,2]*100,col="red",typ="l",lwd=2,ylim=c(0,100.1))
+    plot(bin.midpoints, cred.ints.fdrs[,2]*100,col="red",typ="l",lwd=2,ylim=c(0,100.1), xlab="z",ylab="fdr(%)")
     lines(bin.midpoints, posterior.fdr.means*100,ylim=c(0,100.1))
     lines(bin.midpoints, cred.ints.fdrs[,1]*100,col="blue",typ="l",lwd=2,ylim=c(0,100.1))
   }
 
-  #plot p0 with creds ??
+  fdr.info <- list(
+    fdr.means.func,
+    fdr.medians.func,
+    upper.fdr.ci.func,
+    lower.fdr.ci.func,
+    tdr.means.func,
+    tdr.medians.func,
+    upper.tdr.ci.func,
+    lower.tdr.ci.func,
+    p0,
+    delta0,
+    sigma0)
+  
+  names(fdr.info) <- c("fdr.means.func","fdr.medians.func",
+                       "upper.fdr.ci.func","lower.fdr.ci.func",
+                       "tdr.means.func","tdr.medians.func",
+                       "upper.tdr.ci.func","lower.tdr.ci.func",
+                       "p0","delta0","sigma0")
+  
+  return(fdr.info)
   
   
 }
---
+++ R/zoomed.post.prob.plot.R
@@ -0,0 +1,72 @@
+#--------------------------------------------
+#' @title zoomed.post.prob.plot
+#' @description XXXX
+#' 
+#' @details XXXX
+#'
+#' @param XXXX
+#' 
+#' @return XXXX
+#'
+#' @examples
+#' XXXX
+#--------------------------------------------
+zoomed.post.prob.plot <- function(zscores, zbounds=c(NA,NA), point.est.func, upper.est.func, lower.est.func, prob.scale="percent", xlab=NULL, ylab=NULL, main=NULL) {
+  
+  #put in a check for NAs in zbounds
+  
+  if(is.na(zbounds[1]) & !is.na(zbounds[2])) {
+    
+    zidxs <- which(zscores < zbounds[2])
+    
+  } else if(!is.na(zbounds[1]) & is.na(zbounds[2])) {
+    
+    zidxs <- which(zscores > zbounds[1])
+    
+  } else if(is.na(zbounds[1]) & is.na(zbounds[2])) {
+    
+    zidxs <- 1:length(zscores)
+    
+  }  else if(!is.na(zbounds[1]) & !is.na(zbounds[2])) {
+    
+    zidxs <- which( (zscores >= zbounds[1]) & (zscores <= zbounds[2]) )
+    
+  }
+  
+  zs <- sort(zscores[zidxs])
+  pe <- point.est.func(zs)
+  ue <- upper.est.func(zs)
+  le <- lower.est.func(zs)
+  
+  if(prob.scale=="percent") {
+    pe <- 100 * pe
+    ue <- 100 * ue
+    le <- 100 * le
+  }
+  
+  ymax <- max(ue)
+  ymin <- min(le)
+  
+  if(is.null(xlab)) {
+    xlab.txt <- "z-score"
+  } else {
+    xlab.txt <- xlab
+  }
+  
+  if(is.null(ylab)) {
+    ylab.txt <- "posterior prob."
+  } else {
+    ylab.txt <- ylab
+  }
+  
+  if(is.null(main)) {
+    main.txt <- ""
+  } else {
+    main.txt <- main
+  }
+  
+  plot(zs, pe, typ="l", ylim=c(ymin,ymax), xlab = xlab.txt, ylab=ylab.txt, main=main.txt)
+  lines(zs, ue, col="red")
+  lines(zs, le, col="blue")
+
+}
